fd2754a2dfe4f91d3d99a3d11a42ea40de5b654e
lucki200 <luck_caneo@hotmail.com>
2024-05-09 12:02:25 +0100

    Mensaje descriptivo de los cambios realizados

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const sujeto = ["Mi perro", "Mi gato", "Mi hermano", "Mi vecino", "Mi jefe"];
+const accion = ["se comió", "destruyó", "escondió", "rompió", "olvidó"];
+const objeto = ["mi tarea", "mi almuerzo", "mi teléfono", "mis llaves", "mi computadora"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarExcusa() {
+    const excusa = `${elegirAleatorio(sujeto)} ${elegirAleatorio(accion)} ${elegirAleatorio(objeto)}.`;
+    document.getElementById("excusa").textContent = excusa;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function elegirAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
